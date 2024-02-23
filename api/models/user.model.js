fbebb0b02ba835fe30229e6f117ef776038d786f
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-02-23 16:12:51 +0500

    add admin functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,12 +18,17 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
