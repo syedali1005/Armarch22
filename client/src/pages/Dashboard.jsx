0880c912f20f457e77be483f824d54d2e1729379
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-02-28 17:18:02 +0500

    add dashboard overview to admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
-import DashPosts from '../components/DashPosts';
+import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
-
+import DashboardComp from "../components/DashComp";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -17,17 +17,19 @@ export default function Dashboard() {
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
-      {/* SideBar */}
-      <DashSidebar />
+        {/* SideBar */}
+        <DashSidebar />
       </div>
       {/* Profile */}
-      {tab === 'profile' && <DashProfile />}
-       {/* posts... */}
-       {tab === 'posts' && <DashPosts />}
-       {/* users... */}
-       {tab === 'users' && <DashUsers />}
+      {tab === "profile" && <DashProfile />}
+      {/* posts... */}
+      {tab === "posts" && <DashPosts />}
+      {/* users... */}
+      {tab === "users" && <DashUsers />}
+      {/* dashboard comp */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
 }
