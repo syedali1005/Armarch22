0f363d3a466c0341704e5d10eea2ceb5d82a7cba
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-03-05 16:00:55 +0500

    config files set to origin

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://armarch22.vercel.app/',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
