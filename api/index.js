cc1ae8349696aa6f4046414b9aae39d08fb46112
Abdul Moiz Khan <73781652+AbdulMoizKhan@users.noreply.github.com>
2024-03-06 00:07:23 +0500

    ali_deployment2

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import cookieParser from 'cookie-parser';
+// import userRoutes from './routes/user.route.js';
+// import authRoutes from './routes/auth.route.js';
+// import postRoutes from './routes/post.route.js';
+// import cookieParser from 'cookie-parser';
 // import path from 'path';
 const app = express();
+app.use(express.json());
 
-dotenv.config();
+// dotenv.config();
 
 // mongoose
 //   .connect(process.env.MONGO)
@@ -22,8 +23,7 @@ dotenv.config();
 // const __dirname = path.resolve();
 
 
-app.use(express.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
@@ -39,12 +39,12 @@ app.listen(3000, () => {
 //   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 // });
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || 'Internal Server Error';
+//   res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
