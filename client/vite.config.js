33ed5a94d3d8dc08e7dc6c26ff16938b9b5be733
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-03-05 11:34:34 +0500

    some changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://armarch22.vercel.app/', // Replace this with your backend server URL
-        secure: true, // Change to true if your backend server uses HTTPS
-        changeOrigin: true, // Enable this option if you're proxying to a different origin
-        rewrite: (path) => path.replace(/^\/api/, '') // Optional: Modify this if your backend expects a different path
+        target: 'http://localhost:3000',
+        secure: false,
       },
     },
   },
