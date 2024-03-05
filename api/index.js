2628600a32cc5014aa830d23767ed94bcc18396c
Abdul Moiz Khan <73781652+AbdulMoizKhan@users.noreply.github.com>
2024-03-05 22:40:53 +0500

    ali_deployment2

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+// import path from 'path';
 
 dotenv.config();
 
@@ -18,7 +18,7 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const app = express();
 
