a508e749e48aaeadc36d5ac8a4ea4e2554b86358
Abdul Moiz Khan <73781652+AbdulMoizKhan@users.noreply.github.com>
2024-03-06 00:35:00 +0500

    ali_deployment2

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 // import mongoose from 'mongoose';
 // import dotenv from 'dotenv';
-// import userRoutes from './routes/user.route.js';
-// import authRoutes from './routes/auth.route.js';
-// import postRoutes from './routes/post.route.js';
-// import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import cookieParser from 'cookie-parser';
 // import path from 'path';
 const app = express();
 app.use(express.json());
@@ -23,15 +23,15 @@ app.use(express.json());
 // const __dirname = path.resolve();
 
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-// app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
-// app.use('/api/post', postRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 // app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -39,12 +39,12 @@ app.listen(3000, () => {
 //   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 // });
 
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || 'Internal Server Error';
-//   res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
