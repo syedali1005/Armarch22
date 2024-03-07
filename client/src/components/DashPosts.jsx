ee0f974b7cebfc4b452d83e498727be3eca37b42
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2024-03-08 00:24:57 +0500

    Update DashPosts.jsx

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -14,7 +14,7 @@ export default function DashPosts() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const res = await fetch(`https://armarch22.vercel.app/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
@@ -34,7 +34,7 @@ export default function DashPosts() {
     const startIndex = userPosts.length;
     try {
       const res = await fetch(
-        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+        `https://armarch22.vercel.app/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
       );
       const data = await res.json();
       if (res.ok) {
