3f0cf8f67518fecee5761d2bbf17e5c579403cfb
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2024-03-08 00:11:59 +0500

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -97,7 +97,7 @@ export const google = async (req, res, next) => {
       res
         .status(200)
         .cookie("access_token", token, {
-          httpOnly: true,
+          httpsOnly: true,
         })
         .json(rest);
     }
