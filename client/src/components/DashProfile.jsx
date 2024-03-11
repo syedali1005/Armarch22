01ef8d68ceb943b788b88f9bf6069d6cf7191bf6
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2024-03-11 15:07:46 +0500

    Update DashProfile.jsx

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -107,7 +107,7 @@ export default function DashProfile() {
     }
     try {
       dispatch(updateStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      const res = await fetch(`https://armarch22.vercel.app/api/user/update/${currentUser._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -131,7 +131,7 @@ export default function DashProfile() {
     setShowModal(false);
     try {
       dispatch(deleteUserStart());
-      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      const res = await fetch(`https://armarch22.vercel.app/api/user/delete/${currentUser._id}`, {
         method: "DELETE",
       });
       const data = await res.json();
@@ -147,7 +147,7 @@ export default function DashProfile() {
 
   const handleSignout = async () => {
     try {
-      const res = await fetch("/api/user/signout", {
+      const res = await fetch("https://armarch22.vercel.app/api/user/signout", {
         method: "POST",
       });
       const data = await res.json();
