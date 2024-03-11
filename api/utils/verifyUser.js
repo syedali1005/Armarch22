f083bdfcf5674a5d58805ed188286b63bfa961c4
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2024-03-12 02:58:31 +0500

    Update verifyUser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,16 +1,18 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-
+export const verifyToken = (token,req, next) => {
+  console.log(token);
   if (!token) {
-    return next(errorHandler(401, "Unauthorized"));
+    return next(errorHandler(402, "Unauthorized"));
   }
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return next(errorHandler(401, "Unauthorized"));
     }
-    req.user = user;
-    next();
+    console.log("user", user)
+    req.user = user; 
+    return req.user;
   });
 };
+
