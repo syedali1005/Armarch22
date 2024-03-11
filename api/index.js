bcf2324f2fc54875e60d99605f488d193ca7e2f7
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2024-03-12 02:57:27 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -27,7 +27,9 @@ const __dirname = path.resolve();
 app.use(cookieParser());
 
 // Configure CORS to allow requests from your frontend domain
-app.use(cors());
+app.use(cors({
+  origin: "https://armarch22-frontend.vercel.app"
+}));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
