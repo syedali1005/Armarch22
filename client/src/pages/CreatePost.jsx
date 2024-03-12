e4a1fc3c213d9c08a8f7f1a7422f455b46e09e2c
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2024-03-13 01:31:47 +0500

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -106,6 +106,7 @@ export default function CreatePost() {
             <option value="resedential">Resedential</option>
             <option value="commercial">Commercial</option>
             <option value="towers">Towers</option>
+             <option value="industrial">Industrial</option>
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
